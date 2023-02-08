c074a6cf658a05024c87e2c728f2b7be5829eb79
justin becker <justin3459@hotmail.com>
2023-02-08 14:29:26 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+document.querySelector("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id" , "victory");
+const text = document.getElementById("victory")
+newHeader.innerHTML = "justin is the champion"
+//h1.textContent = "justin is the champion"
+//text.innerHTML = "<p>Justin is the champion</p>"
+document.body.append(newHeader);
+//const addID = .setAttribute("id","victory")
+//document.getElementById("victory").textContent = "Justin is the champion";
